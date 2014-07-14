894f00f0ff886301ba4d9afda66bd37b18e26a0d
David Scherfgen <d.scherfgen@googlemail.com>
2014-07-14 18:55:58 +0200

    Updated many libraries to their current versions, except for ffmpeg.

--- Blocks5/stage.bat
+++ Blocks5/stage.bat
@@ -6,23 +6,25 @@ COPY avformat-52.dll stage
 COPY avutil-50.dll stage
 COPY "Blocks 5 Website.url" stage
 COPY ..\Release\blocks5.exe stage
+tools\upx\upx.exe -9 stage\blocks5.exe
 COPY data.zip stage
 COPY "Donate (de).url" stage
 COPY "Donate (en).url" stage
 COPY hq2x.bat stage
-COPY jpeg.dll stage
 COPY libogg.dll stage
-COPY libpng12-0.dll stage
+COPY libpng15-15.dll stage
 COPY libvorbis.dll stage
 COPY libvorbisfile.dll stage
 COPY makeconfig.bat stage
 COPY oalinst.exe stage
 COPY ogg.dll stage
 COPY ..\Release\pwencrypt.exe stage
+tools\upx\upx.exe -9 stage\pwencrypt.exe
 COPY readme.txt stage
 COPY sdl.dll stage
 COPY sdl_image.dll stage
 COPY ..\Release\showuserdir.exe stage
+tools\upx\upx.exe -9 stage\showuserdir.exe
 COPY swscale-0.dll stage
 COPY update_checker_disable.bat stage
 COPY update_checker_enable.bat stage
